51862c301d9b4495f161b97564488d816e8956f3
davide1217 <davidepizzola17@gmail.com>
2022-04-05 15:36:25 +0200

    completo

---
+++ script.js
@@ -0,0 +1,26 @@
+// password generator
+
+/*
+1 chiedere nome
+2 chiedere cognome
+3 chiedere colore preferito
+4 stampare la password
+*/
+
+const firstName = prompt('Inserisci il tuo nome')
+
+const lastName = prompt('Inserisci il tuo cognome')
+
+const preferedColor = prompt('Inserisci il tuo colore preferito')
+
+console.log(firstName)
+
+console.log(lastName)
+
+console.log(preferedColor)
+
+const password = firstName + lastName + preferedColor + '22'
+
+console.log(password)
+
+document.getElementById('_password').innerHTML = password
